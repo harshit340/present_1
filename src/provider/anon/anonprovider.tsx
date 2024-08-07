ab8d58633cb1dba34fff865fe1d98fc58eb6f04e
harshit340 <harshitshrivastava340@gmail.com>
2024-08-08 02:34:46 +0530

    Switched to real aadhar

--- src/provider/anon/anonprovider.tsx
+++ src/provider/anon/anonprovider.tsx
@@ -5,7 +5,7 @@ import { AnonAadhaarProvider } from "@anon-aadhaar/react";
 const activeChain = "ethereum";
 export default function AnonProvider({ children }: { children: React.ReactNode }) {
     return (
-        <AnonAadhaarProvider _useTestAadhaar={true}>
+        <AnonAadhaarProvider>
             {children}
         </AnonAadhaarProvider>
     );
