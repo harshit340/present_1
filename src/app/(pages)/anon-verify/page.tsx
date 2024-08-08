746576046a47043316102431360fc1aa7070e02c
vibhor <vibgitcode27@gmail.com>
2024-08-09 04:57:48 +0530

    restyled anon-verify

--- src/app/(pages)/anon-verify/page.tsx
+++ src/app/(pages)/anon-verify/page.tsx
@@ -20,8 +20,8 @@ export default function page() {
          <div>
           <div>
           <div style={{marginBottom:"8%"}}>
-                <div style={{display:"flex" , justifyContent:"center" , fontSize:"60px" , marginTop:"7%"}}>
-                    How Does It Exactly Works  
+                <div className="hero text-center mt-8" style={{ marginTop : "100px"}}>
+                  <h1 className="text-globe">How Does It Exactly Works</h1>
                 </div>
                 <div style={{display:"flex" ,flexDirection:"column",alignItems:"center",  justifyContent:"center",textAlign:"center",margin:"2% 8% 2% 8%" , fontSize:"20px"}}>
                     <p>Payu is an advanced decentralized platform that streamlines Ethereum transfers across multiple EVM-based blockchains and various currencies. It incorporates a secure and anonymous Aadhaar-based KYC process, allowing users to onboard quickly and safely while maintaining their privacy. With Payu, transferring digital assets between different blockchain networks becomes effortless and secure.</p>
@@ -29,14 +29,11 @@ export default function page() {
                     <p>One of Payu's key features is its Soundbox, which provides users with real-time audio notifications for their transactions. This ensures that users are always informed about their financial activities, offering immediate updates and enhancing user experience. Additionally, Payu allows users to customize their alerts based on the blockchains they are most interested in, providing tailored and relevant notifications.
                     </p><br></br>
                     <p>By offering seamless multi-chain Ethereum transfers, secure KYC, and customizable real-time transaction alerts, Payu delivers a comprehensive and user-friendly solution for managing digital assets. This ensures users can enjoy a seamless, secure, and informed financial management experience, making Payu an essential tool for cryptocurrency users.</p>
-
-
-
-            </div>
-            </div>
+                </div>
+          </div>
            
 
-           </div>
+        </div>
            
          </div>
        
